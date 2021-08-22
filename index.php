51f065dc4ac6d0902da92d53ec99d0a3b8484c33
yoshinyan <hai_hai_hai_1229@yahoo.co.jp>
2021-08-23 00:02:30 +0900

    css戻す

--- index.php
+++ index.php
@@ -8,6 +8,14 @@ $tasks = $db->all();
 <html lang="ja">
 <head>
     <title>タスク一覧</title>
+    <style type="text/css">
+        table {
+            border-collapse: collapse;
+        }
+        th, td {
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
 <h2>タスク一覧</h2>
