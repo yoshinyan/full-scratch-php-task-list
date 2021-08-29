d9129a4ddbf33f59e1c73703ef2ee41f99b69bb0
yoshinyan <hai_hai_hai_1229@yahoo.co.jp>
2021-08-29 11:19:40 +0900

    日付のvalidation

--- update.php
+++ update.php
@@ -6,6 +6,8 @@ if ($_POST['task_name'] == '') {
     $error_messege = 'タスク名が空欄です';
 } elseif (strlen($_POST['task_name']) > 150) {
     $error_messege = 'タスク名が長すぎます';
+} elseif (!preg_match('/^\d{4}-[01]\d{1}-[012]\d{1}$/', $_POST['done_request_date'])) {
+    $error_messege = '日付の形式が不正です';
 } else {
     $db = new Task();
     $result = $db->update($_POST['id'], $_POST['task_name'], $_POST['done_request_date']);
@@ -21,6 +23,7 @@ if ($_POST['task_name'] == '') {
 <?php if ($error_messege != '') : ?>
     <?= $error_messege ?>
     <form method="post" action="edit.php">
+        <input type="hidden" name="task_name" value="<?= $_POST['task_name'] ?? '' ?>">
         <input type="hidden" name="done_request_date" value="<?= $_POST['done_request_date'] ?? '' ?>">
         <input type="hidden" name="id" value="<?= $_POST['id'] ?? '' ?>">
         <input type="submit" name="return" value="戻る">
